f9647238de82366d01a82d5785374bcb612118a8
kultosh <kulgole90@gmail.com>
2024-09-26 00:19:55 +0545

    Added fullcalendar and fixed UI

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import AppCalendar from './AppCalendar.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(AppCalendar),
 }).$mount('#app')
